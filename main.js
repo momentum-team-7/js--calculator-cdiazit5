d0240a78802f8d0595067737946b1df62fd219f4
cdiazit5 <cdiazit5@gmail.com>
2021-01-28 09:24:54 -0500

    got the buttons to work in the flex box and sized

--- main.js
+++ main.js
@@ -5,9 +5,10 @@ let allOperators = document.querySelectorAll(".operation")
 
 for (let buttons of allButtons) {
     buttons.addEventListener('click', event => {
-        console.log(event.target.class)
-    }
-}
+        console.log(event.target);
+        display.innerHTML = key;
+    });
+        if 
 
 
 
@@ -19,25 +20,8 @@ for (let buttons of allButtons) {
 
 
 
-// let buttonsArray = document.querySelectorAll("button")
-// for (let key of buttonsArray) {
-//     key.addEventListener('click', event => {
-//         console.log(event.target);
-//         display.innerHTML = key;
-//     });
-//         if (event.target.innerHTML === '=') {
-//             console.log(display.innerHTML)
-//             display.innerHTML = eval(display.innerHTML);
-//         }
-//         else if (event.target.innerHTML === 'C') {
-//             display.innerHTML = ' '
-//         }
-//         else if (display.innerHTML === '0' && event.target.innerHTML === '0') {
-//             display.innerHTML = '0'
-//         }
-//         else if (event.target.innerHTML === 'X') {
-//             display.innerHTML = [display.innerHTML, '*'].join('');
-//         }
-//         else {
-//             display.innerHTML = [display.innerHTML, event.target.innerHTML].join('');
-//         }
+
+
+
+
+
