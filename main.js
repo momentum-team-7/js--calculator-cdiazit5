7ed4aa5c552ba0f4f170a1b8b46d7515c0146dec
cdiazit5 <cdiazit5@gmail.com>
2021-01-28 11:11:30 -0500

    implemented input box

--- main.js
+++ main.js
@@ -1,14 +1,36 @@
-let allButtons = document.querySelectorAll(".button")
+let allButtons = document.querySelectorAll('button')
 let allNumbers = document.querySelectorAll(".number")
 let allOperators = document.querySelectorAll(".operation")
+let outputDisplay = document.querySelector(".output")
+let clearButton = document.querySelector('#clear')
+let equalButton = document.querySelector('#equal')
+outputDisplay.innerHTML = ""
+
+// get display to work
+ for (let key of allNumbers) {
+     key.addEventListener('click' event => {
+         console.log(event.target)
+         if (event.target.innerHTML === '=') {
+             console.log(outputDisplay)
+             outputDisplay.innterHTML = eval(outputDisplay.innerHTML);
+         }
+     })
+ }
 
 
-for (let buttons of allButtons) {
-    buttons.addEventListener('click', event => {
-        console.log(event.target);
-        display.innerHTML = key;
-    });
-        if 
+
+
+
+
+
+// collect info from buttons
+
+// for (let buttons of allButtons) {
+//     buttons.addEventListener('click', event => {
+//         console.log()
+//     }
+//     });
+//         if 
 
 
 
